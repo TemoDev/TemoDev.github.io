37a484f64e475f8d637d9678df3f696c5b890014
TemoDev <temo.kuliashvili@gmail.com>
2020-08-17 01:31:49 -0800

    initial commit

--- surfhouse/js/swiper.js
+++ surfhouse/js/swiper.js
@@ -3,7 +3,8 @@ import Swiper from 'https://unpkg.com/swiper/swiper-bundle.esm.browser.min.js';
 const mySwiper = new Swiper('.swiper-container', {
     // Optional parameters
     direction: 'horizontal',
-    loop: true,  
+    loop: false,  
+    slidesPerView:'auto',
     // Navigation arrows
     navigation: {
       nextEl: '.swiper-button-next',
