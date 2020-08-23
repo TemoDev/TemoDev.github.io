8c14dc00b7ee50b0d52b912c10d757d9b9ef88ae
TemoDev <temo.kuliashvili@gmail.com>
2020-08-23 08:08:51 -0800

    initial commit

--- surfhouse/js/swiper.js
+++ surfhouse/js/swiper.js
@@ -8,4 +8,26 @@ const mySwiper = new Swiper('.swiper-container', {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
+  });
+const mySwiperTop = new Swiper('.swiper-container1', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: false,  
+    slidesPerView:'auto',
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next1',
+      prevEl: '.swiper-button-prev1',
+    },
+  })
+const mySwiperSale = new Swiper('.swiper-container2', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: false,  
+    slidesPerView:'auto',
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next2',
+      prevEl: '.swiper-button-prev2',
+    },
   })
