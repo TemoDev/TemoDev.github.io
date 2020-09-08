8a218e2a5624fb3176d3802fe3c279844f64a0b8
TemoDev <temo.kuliashvili@gmail.com>
2020-09-08 03:40:09 -0800

    initial commit

--- sonicfast_ltd/js/swiper.js
+++
@@ -1,9 +0,0 @@
-let swiper = new Swiper('.swiper-container', {
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-      renderBullet: function (index, className) {
-        return '<span class="' + className + '">' + (index + 1) + '</span>';
-      },
-    },
-  });
