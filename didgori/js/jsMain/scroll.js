942a175b403c41326a11ae340324d223b95b7b9b
TemoDev <temo.kuliashvili@gmail.com>
2020-12-04 11:37:37 -0800

    initial commit

--- didgori/js/jsMain/scroll.js
+++ didgori/js/jsMain/scroll.js
@@ -8,9 +8,6 @@ navLink[1].addEventListener('click', ()=>{
 navLink[2].addEventListener('click', ()=>{
     gsap.to(window, {duration:1.0, scrollTo: '.offers',})
 })
-navLink[3].addEventListener('click', ()=>{
-    gsap.to(window, {duration:1.0, scrollTo: '.production',})
-})
-navLink[5].addEventListener('click', ()=>{
+navLink[4].addEventListener('click', ()=>{
     gsap.to(window, {duration:1.0, scrollTo: '.contact',})
 })
